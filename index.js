38b999656d0f016666d953b271619af1d1da57a2
lewisky <lewisky28mawira@gmail.com>
2023-03-21 08:21:54 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let response;
+  if (feet <= 400) {
+    response = 'This one is on me!';
+  } else if (feet > 2000) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    response = 'No can do.';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sound good.' :'No go.';
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
